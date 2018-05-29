7c969a0c35a8f3dbf28d5ccb83f9905f77a6002f
ktr0731 <aizu.s1230022@gmail.com>
2018-05-29 21:30:43 +0900

    add module example

---
+++ phrases/src/main.rs
@@ -0,0 +1,12 @@
+extern crate phrases as sayings;
+
+use sayings::english::{farewells, greetings};
+use sayings::japanese;
+
+fn main() {
+    println!("Hello in English: {}", greetings::hello());
+    println!("Goodbye in English: {}", farewells::goodbye());
+
+    println!("Hello in Japanese: {}", japanese::hello());
+    println!("Goodbye in Japanese: {}", japanese::goodbye());
+}
